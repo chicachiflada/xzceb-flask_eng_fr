66218a6aa5460e1d29fa1e851a135b8bda899f26
chicachiflada <gabby.trofin@gmail.com>
2022-11-09 03:56:34 -0500

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -21,6 +21,7 @@ def french_to_english():
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == "__main__"
     app.run(host="0.0.0.0", port=8080)
